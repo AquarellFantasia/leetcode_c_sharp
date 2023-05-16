7c29f383e7fe655965cfcae2c7326b5fd77a9d62
AquarellFantasia <em224342@gmail.com>
2023-05-16 15:28:24 +0200

    added 33 leetcode task

---
+++ app/C_33_Search_in_Rotated_Sorted_Array.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace SolutionNamespace{
+    class C_33_Search_in_Rotated_Sorted_Array{
+        public static int Search(int[] nums, int target) {
+            return 0;
+        }
+    }
+}
