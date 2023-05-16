5ec6ebed449af019e122dc2e2f9b45e403d055e3
AquarellFantasia <em224342@gmail.com>
2023-05-16 17:59:35 +0200

    Solved 33

--- app/Program.cs
+++ app/Program.cs
@@ -7,13 +7,17 @@ namespace MainNamespace
     {
         public static void Main(string[] args)
         {
-            TreeNode root = new TreeNode(3);
-            root.left = new TreeNode(9);
-            root.right = new TreeNode(20);
-            root.right.left = new TreeNode(15);
-            root.right.right = new TreeNode(7);
-            Console.WriteLine("Answer is:" + C_111_Minimum_Depth_of_Binary_Tree.MinDepth(root) + " while expected: 3");
-            // Console.WriteLine("Answer is:" + C_102_Binary_Tree_Level_Order_Traversal.LevelOrder(root) + " while expected: 49");
+            int[] arr1 = new int[] {0, 1, 2, 3, 4, 5, 6, 7 };
+            int[] arr2 = new int[] {4,5,6,7,0,1,2 };
+            int[] arr3 = new int[] {1};
+            int[] arr4 = new int[] {};
+            int[] arr5 = new int[] {4,5,6,7,8,1,2,3};
+            Console.WriteLine("Answer is:" + C_33_Search_in_Rotated_Sorted_Array.Search(arr1, 1) + " while expected: 1");
+            Console.WriteLine("Answer is:" + C_33_Search_in_Rotated_Sorted_Array.Search(arr2, 0) + " while expected: 4");
+            Console.WriteLine("Answer is:" + C_33_Search_in_Rotated_Sorted_Array.Search(arr3, 1) + " while expected: 0");
+            Console.WriteLine("Answer is:" + C_33_Search_in_Rotated_Sorted_Array.Search(arr4, 1) + " while expected: -1");
+            Console.WriteLine("Answer is:" + C_33_Search_in_Rotated_Sorted_Array.Search(arr2, 5) + " while expected: 1");
+            Console.WriteLine("Answer is:" + C_33_Search_in_Rotated_Sorted_Array.Search(arr5, 8) + " while expected: 4");
         }
 
          public static string PrintArray(int[] arr){
